887ee8364358665205649a50028d01969fa3b06a
Delaney Fogarty <delaney.fk@gmail.com>
2022-06-07 10:21:02 -0700

    wrote tests for zodiac

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,13 +2,22 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const { zodiacs } = require('../data/zodiac');
 
 describe('backend-express-template routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+  it('/zodiacs should return a list of zodiacs', async () => {
+    const res = await request(app).get('/zodiacs');
+    expect(res.body).toEqual(zodiacs);
+  });
+  it('/zodiacs/:id should return zodiac details', async () => {
+    const res = await request(app).get('/zodiacs/10');
+    expect(res.body).toEqual({
+      id: '10',
+      name: 'scorpio',
+    });
   });
   afterAll(() => {
     pool.end();
